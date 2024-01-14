8b1082310af10e7e26eeb4ad00be00542d564b4a
vinay kumar sharma <royalpremsharma@gmail.com>
2024-01-14 23:13:30 +0530

    completed..

--- chapter02/largest_number.java
+++ chapter02/largest_number.java
@@ -1,8 +1,24 @@
 package chapter02;
+import java.util.*;
 
 public class largest_number {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int num1=sc.nextInt();
+        System.out.println("Enter second number : ");
+        int num2=sc.nextInt();
+
+        if(num1>num2)
+        {
+            System.out.println(num1+" is largest.");
+        }
+        else{
+            System.out.println(num2+" is largest.");
+
+        }
+        sc.close();
     }
 }
